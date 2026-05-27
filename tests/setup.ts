e4a5117f672c42d8c8228e8c7a1e5a8dbf85da05
Thando Mini <thando.mini@sanlam.co.za>
2026-05-27 22:42:09 +0200

    test: msw harness for integration tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom/vitest";
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { server } from "./integration/msw";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
